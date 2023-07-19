4f6677812a2522380e0553b802174b061a380af5
Zyta <timer217@gmail.com>
2023-07-19 17:47:22 +0700

    change js to ts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,5 @@
-<script>
-	const today = new Date();
-</script>
-
 <div class="flex-grow flex flex-col items-center justify-center">
 	<div class="container mx-auto text-center h-full">
-		<h1 class="text-7xl">Today {today.getDay() === 6 ? 'is' : 'is not'} Friday</h1>
+		<h1 class="text-7xl">Hi, I'm Zyta</h1>
 	</div>
 </div>
