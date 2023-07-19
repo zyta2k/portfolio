ce3ae962d4d309cf3639b38c6adf4d4f54c7ea13
Zyta <timer217@gmail.com>
2023-07-19 17:27:58 +0700

    re-re-initialize project

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nuxt-layout>
-    <nuxt-page></nuxt-page>
-  </nuxt-layout>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss"></style>
--- components/layout/header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container mx-auto flex"></div>
-</template>
-
-<script setup lang="ts"></script>
