220952593da92ab8625a1ef6fc735a53ab5442c1
Zyta <timer217@gmail.com>
2023-07-19 17:24:49 +0700

    re-initialize project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<div class="min-h-screen flex flex-col bg-neutral-100">
+	<!-- <div>header</div> -->
+	<div class="flex-grow flex flex-col">
+		<slot />
+	</div>
+	<!-- <div>footer</div> -->
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	const today = new Date();
+</script>
+
+<div class="flex-grow flex flex-col items-center justify-center">
+	<div class="container mx-auto text-center h-full">
+		<h1 class="text-7xl">Today {today.getDay() === 6 ? 'is' : 'is not'} Friday</h1>
+	</div>
+</div>
